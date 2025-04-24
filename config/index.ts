9980f26b474e6b7b6116280bff66711fc2733393
张亿杰 <14846761+zhang-yijie700317@user.noreply.gitee.com>
2025-04-24 23:43:22 +0800

    首页完成

--- config/index.ts
+++ config/index.ts
@@ -30,8 +30,8 @@ export default defineConfig<'webpack5'>(async (merge, { command, mode }) => {
     defineConstants: {
       'process.env.NODE_ENV': JSON.stringify('development'),
       'process.env.API_MOCK_URL': JSON.stringify('http://localhost:4523/m1/6225353-5918947-default'),
-      'process.env.API_BASE_URL': JSON.stringify('http://localhost:3000/api'),
-      'process.env.USE_MOCK': JSON.stringify(true)
+      'process.env.API_BASE_URL': JSON.stringify('http://8.140.200.27:8080'),
+      'process.env.USE_MOCK': JSON.stringify(false)
     },
     copy: {
       patterns: [
--- src/pages/map/index/index.vue
+++ src/pages/map/index/index.vue
@@ -1,73 +1,73 @@
 <template>
-  <view class="container">
+<!--   <web-view src="http://localhost:5173/"  @message="handleMessageFromWeb"-->
+<!--   bindmessage="handleMessageFromWeb"/>-->
+     <web-view src="http://8.140.200.27:8087/"  @message="handleMessageFromWeb"
+   bindmessage="handleMessageFromWeb"/>
+<!--  <view class="container">-->
 
-    <!-- 顶部搜索栏 -->
-    <view class="search-bar">
-      <view class="search-box">
-        <input
-          class="search-input"
-          placeholder="搜索教室"
-          v-model="searchText"
-          @focus="showHistory = true"
-          @blur="handleBlur"
-        />
-        <view class="search-btn" >搜索</view>
-      </view>
+<!--    &lt;!&ndash; 顶部搜索栏 &ndash;&gt;-->
+<!--    <view class="search-bar">-->
+<!--      <view class="search-box">-->
+<!--        <input-->
+<!--          class="search-input"-->
+<!--          placeholder="搜索教室"-->
+<!--          v-model="searchText"-->
+<!--          @focus="showHistory = true"-->
+<!--          @blur="handleBlur"-->
+<!--        />-->
+<!--        <view class="search-btn" >搜索</view>-->
+<!--      </view>-->
+<!--    </view>-->
 
-    </view>
 
+<!--    <view class="floor-selector" @tap.stop>-->
+<!--      &lt;!&ndash; 左边楼层栏 &ndash;&gt;-->
+<!--      <view class="floor-list">-->
+<!--        <view-->
+<!--          v-for="floor in floors"-->
+<!--          :key="floor"-->
+<!--          :class="['floor-item', selectedFloor === floor ? 'active' : '']"-->
+<!--          @tap="toggleBuildingList(floor)"-->
+<!--        >-->
+<!--          <text>{{ floor }}</text>-->
+<!--          <text class="arrow">›</text>-->
+<!--        </view>-->
+<!--      </view>-->
 
-     <web-view src="http://8.140.200.27:8087/"     @message="handleMessageFromWeb"
-    :bindmessage="handleMessageFromWeb"/>
+<!--      &lt;!&ndash; 右侧楼栋弹出 &ndash;&gt;-->
+<!--      <view-->
+<!--        v-if="showBuildings"-->
+<!--        class="building-popup"-->
+<!--      >-->
+<!--        <scroll-view scroll-y="true" class="building-scroll">-->
+<!--          <view-->
+<!--            v-for="b in buildings[selectedFloor] || []"-->
+<!--            :key="b"-->
+<!--            class="building-item"-->
+<!--            @tap="selectBuilding(b)"-->
+<!--          >-->
+<!--            {{ b }}-->
+<!--          </view>-->
+<!--        </scroll-view>-->
+<!--      </view>-->
+<!--    </view>-->
 
-    <view class="floor-selector" @tap.stop>
-      <!-- 左边楼层栏 -->
-      <view class="floor-list">
-        <view
-          v-for="floor in floors"
-          :key="floor"
-          :class="['floor-item', selectedFloor === floor ? 'active' : '']"
-          @tap="toggleBuildingList(floor)"
-        >
-          <text>{{ floor }}</text>
-          <text class="arrow">›</text>
-        </view>
-      </view>
+<!--    <div v-if="selectedPlace" class="place-card">-->
+<!--      <div class="place-name">{{ selectedPlace.name }}</div>-->
+<!--      <div class="place-desc">{{ selectedPlace.description }}</div>-->
+<!--      <div class="actions">-->
+<!--        <button class="action-button" open-type="collect">-->
+<!--          <image src="/assets/icons/收藏.png" class="icon"/>-->
+<!--          收藏-->
+<!--        </button>-->
+<!--        <button class="action-button" bindtap="navigateToLocation">-->
+<!--          <image src="/assets/icons/导航.png" class="icon"/>-->
+<!--          去这里-->
+<!--        </button>-->
+<!--      </div>-->
+<!--    </div>-->
 
-      <!-- 右侧楼栋弹出 -->
-      <view
-        v-if="showBuildings"
-        class="building-popup"
-      >
-        <scroll-view scroll-y="true" class="building-scroll">
-          <view
-            v-for="b in buildings[selectedFloor] || []"
-            :key="b"
-            class="building-item"
-            @tap="selectBuilding(b)"
-          >
-            {{ b }}
-          </view>
-        </scroll-view>
-      </view>
-    </view>
-
-    <div v-if="selectedPlace" class="place-card">
-      <div class="place-name">{{ selectedPlace.name }}</div>
-      <div class="place-desc">{{ selectedPlace.description }}</div>
-      <div class="actions">
-        <button class="action-button" open-type="collect">
-          <image src="/assets/icons/收藏.png" class="icon"/>
-          收藏
-        </button>
-        <button class="action-button" bindtap="navigateToLocation">
-          <image src="/assets/icons/导航.png" class="icon"/>
-          去这里
-        </button>
-      </div>
-    </div>
-
-  </view>
+<!--  </view>-->
 </template>
 
 
@@ -80,14 +80,30 @@ import wayIcon from 'src/assets/icons/导航.png'
 
 
 
-let canvas, ctx
-let imageRef = null // ✅ 全局保存图片对象
-const position = ref({x: 0, y: 0, scale: 1})
 
-
+// Page({
+//   handleMessageFromWeb(e){
+//   console.log(123)
+//   const data = e.detail.data[0]
+//   const { type, message } = e.detail.data;
+//   if(message==="搜索"){
+//   Taro.navigateTo({
+//     url: '/pages/history/index/index',
+//   })
+//   }
+//   console.log('收到来自 web 的消息：', data)
+// }
+// })
 
 const handleMessageFromWeb = (e) => {
+  console.log(123)
   const data = e.detail.data[0]
+  const { type, message } = e.detail.data;
+  if(message==="搜索"){
+  Taro.navigateTo({
+    url: '/pages/history/index/index',
+  })
+  }
   console.log('收到来自 web 的消息：', data)
 }
 
