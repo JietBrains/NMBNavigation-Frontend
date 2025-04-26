473071022a2dd8b01e0b9142002a341824a46f19
hjzts <119279415+hjzts@users.noreply.github.com>
2025-04-27 00:02:14 +0800

    test🚦:/

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,10 +1,44 @@
 <template>
+    <view class="login">
+        <view class="login-container">
+            <view class="avatar" style="display: flex; justify-content: center; align-items: center;">
+                <nut-avatar size="large" bg-color="pink">
+                    <My />
+                </nut-avatar>
+            </view>
+            <view class="title" style="text-align: center;">Hi,欢迎登录</view>
+
+            <view class="form">
+                <view class="input-wrapper">
+                    <Add class="input-icon" />
+                    <nut-input class="input-item" v-model="username" placeholder="请输入账号" :border="false">
+                    </nut-input>
+                </view>
+
+                <view class="input-wrapper">
+                    <Find class="input-icon" />
+                    <nut-input class="input-item" v-model="password" placeholder="请输入密码" type="password" :border="false">
+                    </nut-input>
+                </view>
+
+                <nut-space direction="vertical" fill>
+                    <nut-button type="primary" plain block @click="getUserProfile">
+                        获取个人信息
+                    </nut-button>
+
+                    <nut-button type="primary" plain block @click="handleWechatLogin">
+                        使用微信登录
+                    </nut-button>
+                </nut-space>
+
+            </view>
+        </view>
+    </view>
     <view class="container">
         <view class="userinfo">
             <template v-if="!hasUserInfo">
                 <button v-if="canIUseGetUserProfile" @tap="getUserProfile">获取头像昵称</button>
-                <!-- <button v-else open-type="getUserInfo" @getuserinfo="getUserInfo">获取头像昵称</button> -->
-                <button v-else open-type="getUserProfile" @getuserinfo="getUserProfile"> 获取头像昵称 </button>
+                <button v-else open-type="getUserInfo" @getuserinfo="getUserInfo">获取头像昵称</button>
             </template>
             <template v-else>
                 <image class="userinfo-avatar" :src="userProfile.userInfo.avatarUrl" mode="cover" @tap="bindViewTap" />
@@ -13,7 +47,7 @@
         </view>
     </view>
 </template>
-  
+
 <script lang="ts" setup>
 import { reactive, ref, onMounted } from 'vue'
 import Taro from '@tarojs/taro'
@@ -55,6 +89,9 @@ const userProfile = reactive<UserProfile>({
         province: ''
     }
 })
+const username = ref('')
+const password = ref('')
+
 
 const hasUserInfo = ref(false)
 const canIUseGetUserProfile = ref(false)
@@ -63,28 +100,33 @@ onMounted(() => {
     canIUseGetUserProfile.value = true
 })
 
-// const getUserProfile = () => {
-//     console.log('获取用户信息——getUserProfile')
-//     Taro.getUserProfile({
-//         desc: '用于完善会员资料',
-//         success: (res) => {
-//             console.log('获取用户信息成功', res)
-//             Object.assign(userProfile, res)
-//             hasUserInfo.value = true
-//         },
-//         fail: (err) => {
-//             console.error('获取用户信息失败', err)
-//         },
-//         complete: (res) => {
-//             console.log('获取用户信息完成', res)
-//         }
-//     })
-// }
+const getUserProfile = () => {
+    console.log('获取用户信息——getUserProfile')
+    Taro.getUserProfile({
+        desc: '用于完善会员资料',
+        success: (res) => {
+            console.log('获取用户信息成功', res)
+            Object.assign(userProfile, res)
+            hasUserInfo.value = true
+        },
+        fail: (err) => {
+            console.error('获取用户信息失败', err)
+        },
+        complete: (res) => {
+            console.log('获取用户信息完成', res)
+        }
+    })
+}
 
-const getUserProfile = (e: any) => {
-    console.log('真实用户信息', e.detail.userInfo)
-    Object.assign(userProfile.userInfo, e.detail.userInfo)
-    hasUserInfo.value = true
+const handleWechatLogin = async () => {
+    try {
+        const { code } = await wx.login()
+        console.log('微信登录 code:', code)
+
+        console.log('微信登录')
+    } catch (error) {
+        console.error('微信登录失败:', error)
+    }
 }
 
 const getUserInfo = (e: any) => {
