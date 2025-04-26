dc8bdcdb9322a00eeb33a47db4961641cdf8bbe0
hjzts <119279415+hjzts@users.noreply.github.com>
2025-04-26 21:41:45 +0800

    feat✨://

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,18 +1,19 @@
 <template>
-  <view class="index">
-    <Counter />
-    
-  </view>
+    <view class="index">
+        <Counter />
+    </view>
+    <Tabbar></Tabbar>
 </template>
 
 <script>
 import './index.scss'
 import Counter from '../../components/Counter.vue'
+import Tabbar from '../../components/Tabbar.vue'
 
 export default {
-  name: 'Index',
-  components: {
-    Counter
-  }
+    name: 'Index',
+    components: {
+        Counter
+    }
 }
 </script>
